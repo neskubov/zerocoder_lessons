7ea42ee9b67a08ba895e33e8d2c5b7796417dbde
neskubov <51290924+neskubov@users.noreply.github.com>
2025-06-05 23:06:36 +0300

    add PS06

--- PS06_srapy_SQLlite/divanParsWriteTosSqllite/divanParsWriteTosSqllite/pipelines.py
+++ PS06_srapy_SQLlite/divanParsWriteTosSqllite/divanParsWriteTosSqllite/pipelines.py
@@ -6,8 +6,31 @@
 
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
-
+import sqlite3
 
 class DivanparswritetossqllitePipeline:
+
+    def __init__(self):
+        self.connection = sqlite3.connect('scrapy_data.db')
+        self.cursor = self.connection.cursor()
+
+        self.cursor.execute("""
+            CREATE TABLE IF NOT EXISTS items (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                title TEXT,
+                price TEXT,
+                url TEXT
+            )
+        """)
+        self.connection.commit()
+
     def process_item(self, item, spider):
+        self.cursor.execute("""
+            INSERT INTO items (title, price, url) VALUES (?, ?, ?)
+        """, (item['title'], item['price'], item['url']))
+        self.connection.commit()
         return item
+
+    def close_spider(self, spider):
+        # Закрытие соединения с базой данных
+        self.connection.close()
