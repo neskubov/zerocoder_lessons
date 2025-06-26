de67b3f247857e4ea9cb0d8876b05619c96b2c14
neskubov <51290924+neskubov@users.noreply.github.com>
2025-06-26 10:07:03 +0300

    выполненное задание

--- VD04. Flask/main.py
+++ VD04. Flask/main.py
@@ -1,10 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template
+from datetime import datetime
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello, World!"
+@app.route('/')
+def index():
+    current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    return render_template('index.html', current_time=current_time)
+
+@app.route('/blog')
+def blog():
+    return render_template('blog.html')
+
+@app.route('/contacts')
+def contacts():
+    return render_template('contacts.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
