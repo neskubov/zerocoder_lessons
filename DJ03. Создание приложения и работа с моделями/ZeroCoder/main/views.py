87243470f0e91fc7cea8ae4c9e9eb00185df94e0
neskubov <51290924+neskubov@users.noreply.github.com>
2025-08-13 20:08:58 +0300

    add code for lesson "DJ03. Создание приложения и работа с моделями"

---
+++ DJ03. Создание приложения и работа с моделями/ZeroCoder/main/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+# Create your views here.
+
+from django.http import HttpResponse
+
+def index(request):
+    return render(request, 'main/index.html')
+
+def data(request):
+    return render(request, 'main/data.html')
+
+def test(request):
+    return render(request, 'main/test.html')
+
+
+
---
+++ DJ03. Создание приложения и работа с моделями/ZeroCoder/news/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import News_post
+
+admin.site.register(News_post)
+
+
+# Register your models here.
