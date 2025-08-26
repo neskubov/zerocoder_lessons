91fb6803d16caca3e739939af332dc1efaee0fc4
neskubov <51290924+neskubov@users.noreply.github.com>
2025-08-26 13:53:55 +0300

    add code for lesson TG04.3
    Добавлен новый функционал, изменены названия переменных

--- TG04. Реализация меню и кнопок. Работа с интерфейсами/keyboard.py
+++ TG04. Реализация меню и кнопок. Работа с интерфейсами/keyboard.py
@@ -1,6 +1,6 @@
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton
 
-keyboard_main = ReplyKeyboardMarkup(
+menu_start = ReplyKeyboardMarkup(
                 keyboard=[
                     [
                         KeyboardButton(text=f'Привет'),
@@ -8,3 +8,27 @@ keyboard_main = ReplyKeyboardMarkup(
                     ]
                 ],
                 resize_keyboard=True)
+
+menu_multimedia = InlineKeyboardMarkup(
+                        inline_keyboard=[
+                            [
+                                InlineKeyboardButton(text='Новости 📰', url='https://news.com'),
+                                InlineKeyboardButton(text='Музыка 🎵', url='https://music.com'),
+                                InlineKeyboardButton(text='Видео 🎬', url='https://video.com')
+                            ]
+                        ])
+
+dynamic_menu = InlineKeyboardMarkup(
+                   inline_keyboard=[
+                        [
+                            InlineKeyboardButton(text=f'Показать больше', callback_data="show_more")
+                        ]
+                    ])
+
+menu_options = InlineKeyboardMarkup(
+                   inline_keyboard=[
+                        [
+                           InlineKeyboardButton(text="Опция 1", callback_data="option_1"),
+                           InlineKeyboardButton(text="Опция 2", callback_data="option_2")
+                        ]
+                    ])
--- TG04. Реализация меню и кнопок. Работа с интерфейсами/main.py
+++ TG04. Реализация меню и кнопок. Работа с интерфейсами/main.py
@@ -2,11 +2,11 @@ import asyncio
 import logging
 import os
 from aiogram import Bot, Dispatcher, F
-from aiogram.filters import CommandStart
+from aiogram.filters import CommandStart, Command
 from aiogram.fsm.storage.memory import MemoryStorage
 from aiogram.types import Message, CallbackQuery
 from dotenv import load_dotenv
-from keyboard import keyboard_main
+from keyboard import menu_start, menu_multimedia, dynamic_menu, menu_options
 
 load_dotenv()
 TOKEN = os.getenv("BOT_TOKEN")
@@ -18,8 +18,35 @@ logging.basicConfig(level=logging.INFO)
 
 
 @dp.message(CommandStart())
-async def start(message: Message):
-    await message.answer('Hi',reply_markup=keyboard_main)
+async def command_start(message: Message):
+    await message.answer('Это учебный проект: "Работа с интерфесами в телеграм"\n'
+                         'Введите /help для получения списка команд',
+                         reply_markup=menu_start)
+
+
+@dp.message(Command('help'))
+async def command_help(message: Message):
+    await message.answer(
+                        '/start - Запуск бота и стартовой клавиатуры\n'
+                        '/help - Справка по командам\n'
+                        '/links - Меню мультимедиа\n'
+                        '/dynamic - Динамическое меню'
+                        )
+
+
+@dp.message(Command('links'))
+async def get_menu_multimedia(message: Message):
+    await message.answer('Выбери раздел:', reply_markup=menu_multimedia)
+
+
+@dp.message(Command('dynamic'))
+async def get_dynamic_menu(message: Message):
+    await message.answer('Динамическое меню',reply_markup=dynamic_menu)
+
+
+@dp.callback_query(F.data == 'show_more')
+async def get_menu_options(callback: CallbackQuery):
+    await callback.message.edit_text('Выбери опцию', reply_markup=menu_options)
 
 
 @dp.message()
